f5006c974270c085ed2fc2d70d70dfc2de33727f
Mohammedelsayed412 <mohammedelsayed41298@gmail.com>
2025-04-23 21:56:03 +0200

    add about lazy

---
+++ src/routes/about/about.lazy.tsx
@@ -0,0 +1,8 @@
+export function RouteComponent() {
+  // TODO ==> how to get loader data here ?
+  return (
+    <div>
+      <div className="text-blue-500 text-xl">Hello "/about"!</div>
+    </div>
+  );
+}
--- src/routes/about/about.tsx
+++ src/routes/about/about.tsx
@@ -1,7 +1,13 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import {
+  createFileRoute,
+  lazyRouteComponent,
+  Link,
+} from "@tanstack/react-router";
 import { aboutServerFn } from "./actions";
 export const Route = createFileRoute("/_NavigationLayout/about")({
-  component: RouteComponent,
+  component: lazyRouteComponent(() =>
+    import("./about.lazy").then((mod) => mod.RouteComponent)
+  ),
   loader: async () => {
     await aboutServerFn();
     return {
@@ -9,11 +15,3 @@ export const Route = createFileRoute("/_NavigationLayout/about")({
     };
   },
 });
-
-function RouteComponent() {
-  return (
-    <div>
-      <div className="text-blue-500 text-xl">Hello "/about"!</div>
-    </div>
-  );
-}
